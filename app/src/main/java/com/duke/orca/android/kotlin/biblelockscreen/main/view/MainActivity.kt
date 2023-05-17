0fb2ab73d25e207931c1bd0948c5d28d294c458c
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-05-18 08:44:15 +0900

    Cleanup

--- app/src/main/java/com/duke/orca/android/kotlin/biblelockscreen/main/view/MainActivity.kt
+++ app/src/main/java/com/duke/orca/android/kotlin/biblelockscreen/main/view/MainActivity.kt
@@ -3,7 +3,6 @@ package com.duke.orca.android.kotlin.biblelockscreen.main.view
 import android.app.ActivityOptions
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.provider.Settings
